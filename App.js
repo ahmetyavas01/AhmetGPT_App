0e96ee22189052862d93160bab114f78435655b3
Ahmet Yavas <ahmetyavas1940@gmail.com>
2024-04-30 02:59:12 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, Text, Alert, StyleSheet, ScrollView, StatusBar, SafeAreaView, Pressable } from 'react-native';
+import { View, TextInput, Button, Text, Alert, StyleSheet, ScrollView, StatusBar, SafeAreaView, ActivityIndicator, TouchableOpacity } from 'react-native';
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import { Ionicons } from '@expo/vector-icons';
 
 const API_KEY = 'API_KEY';
 const genAI = new GoogleGenerativeAI(API_KEY);
@@ -8,29 +9,34 @@ const genAI = new GoogleGenerativeAI(API_KEY);
 const App = () => {
   const [prompt, setPrompt] = useState('');
   const [response, setResponse] = useState('');
+  const [loading, setLoading] = useState(false);
 
   const generateText = async (prompt) => {
+    setLoading(true);
     try {
       const model = genAI.getGenerativeModel({ model: "gemini-pro" });
       const result = await model.generateContent(prompt);
       const text = await result.response.text();
-
       setResponse(text);
     } catch (error) {
-      console.error('API isteği başarısız:', error);
-      Alert.alert('Hata', 'Metin oluşturulamadı. Lütfen daha sonra tekrar deneyin.');
+      console.error('API request failed:', error);
+      Alert.alert('Error', 'Text could not be generated. Please try again later.');
+    } finally {
+      setLoading(false);
     }
   };
 
   const handleGenerateText = () => {
     generateText(prompt);
   };
+
   return (
     <SafeAreaView style={styles.container}>
+
       <View>
         <Text style={styles.headerText}>Welcome to AhmetGPT 👋</Text>
       </View>
-      <View>
+      <View style={styles.inputContainer}>
         <TextInput
           style={styles.TextInput}
           placeholder="Write your message."
@@ -38,27 +44,36 @@ const App = () => {
           value={prompt}
           placeholderTextColor={'gray'}
           autoCapitalize='none'
-          autoCorrect={false}
         />
+        <View style={{
+          paddingTop: 17,
+          marginLeft: 10
+        }}>
+          <TouchableOpacity onPress={handleGenerateText}>
+            <Ionicons name="send" size={25} color="gray" />
+          </TouchableOpacity>
+        </View>
       </View>
-      <View style={{ flexDirection: 'row', alignSelf: 'center' }}>
-        <Button title="Send" onPress={handleGenerateText} />
-        
-      </View>
-      {response !== '' &&
+
+      {loading ? (
+        <View style={styles.activityIndicatorContainer}>
+          <ActivityIndicator size={'large'} color={'#121481'} />
+        </View>
+      ) : response !== '' ? (
         <ScrollView>
           <Text style={styles.resultText}>{response}</Text>
         </ScrollView>
-      }
+      ) : null}
+
       <StatusBar style='light' />
     </SafeAreaView>
   );
 };
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'rgb(17, 22, 27)',
-
   },
   headerText: {
     fontSize: 25,
@@ -71,30 +86,37 @@ const styles = StyleSheet.create({
   TextInput: {
     width: '90%',
     height: 50,
-    paddingHorizontal: 20,
+    paddingHorizontal: 17,
     paddingVertical: 12,
-    borderRadius: 15,
-    margin: 8,
+    borderRadius: 22,
     backgroundColor: 'rgb(51, 59, 76)',
-    fontSize: 16,
+    fontSize: 20,
     color: 'white',
     marginTop: 20,
     alignSelf: 'center'
   },
   resultText: {
     fontSize: 20,
-    fontWeight: 600,
+    fontWeight: '500',
     color: 'white',
     marginHorizontal: 35,
-    marginVertical: 12
-  },
-  copyButton: {
-    fontSize: 16,
-    color: 'white',
-    alignSelf: 'center',
     marginVertical: 20
-  }
-});
+  },
+  activityIndicatorContainer: {
+    flex:0.7,
+    justifyContent: 'center',
+    alignItems: 'center',
 
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '90%',
+    alignSelf: 'center',
+    marginTop: 10,
+  }
+
+});
 
 export default App;
